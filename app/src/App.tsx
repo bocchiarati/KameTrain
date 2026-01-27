612627171544049422c459f8a0fb54e5ceb05b5e
suki <88000sofien@gmail.com>
2026-01-27 15:28:57 +0100

    🎉 Init React Front-End App

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return <div className="main-container">
+                <h1>Hello World ! 🏋️‍♂️</h1>
+                <p>Bienvenue sur <strong>KameTrain</strong>.</p>
+                <p>C'est ici que tu vas enregistrer tes futures séances.</p>
+           </div>
+
+}
+export default App
