ecf791303aea3065902716112b02f9bab0e1239b
Ewen <wenwengilbert@gmail.com>
2026-01-29 00:52:39 +0100

    🚑️ Fix missing Request & Response from express import in controller muscle group

--- api/src/controllers/muscleGroupController.ts
+++ api/src/controllers/muscleGroupController.ts
@@ -1,3 +1,4 @@
+import type {Request, Response} from 'express';
 import {muscleGroupService} from "../services/muscleGroupService.js";
 
 export const getMuscleGroup = async (req: Request, res: Response) => {
