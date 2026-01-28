ab8846ce3b49525cb8f815555acdac543b5ceea4
Ewen <wenwengilbert@gmail.com>
2026-01-29 00:46:52 +0100

    🎨 Exercises Service: Create getExerciseById Function

--- api/src/services/exercisesService.ts
+++ api/src/services/exercisesService.ts
@@ -4,5 +4,8 @@ import {exercisesRepository} from "../repositories/exercisesRepository.js";
 export const exercisesService = {
     getAllExercises: async (): Promise<Exercise[]> => {
         return await exercisesRepository.findAll() as Exercise[]
+    },
+    getExerciseById: async (id: string): Promise<Exercise | null> => {
+        return await exercisesRepository.findById(id) as Exercise | null
     }
 }
