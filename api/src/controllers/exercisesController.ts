c12146756e181462dc2a6c06d889d3373c39d0bc
Ewen <wenwengilbert@gmail.com>
2026-01-29 00:50:24 +0100

    🎨 Exercises Controller : Create getExercise Action

--- api/src/controllers/exercisesController.ts
+++ api/src/controllers/exercisesController.ts
@@ -1,10 +1,25 @@
+import type {Request, Response} from 'express';
 import {exercisesService} from "../services/exercisesService.js";
 
-export const getExercises = async (req: Request, res: Response) => {
-    try {
-        const exercises = await exercisesService.getAllExercises();
-        return res.json(exercises);
-    } catch (error) {
-        return res.status(500).json({ error : error });
+export const exercisesController = {
+    getExercises : async (req: Request, res: Response) => {
+        try {
+            const exercises = await exercisesService.getAllExercises();
+            return res.status(200).json(exercises);
+        } catch (error) {
+            return res.status(500).json({ error : error });
+        }
+    },
+    getExercise : async (req: Request, res: Response) => {
+        const { id } = req.params;
+        try {
+            const exercise = await exercisesService.getExerciseById(id as string);
+            if (!exercise) {
+                return res.status(404).json({ error: 'Exercise not found' });
+            }
+            return res.json(exercise);
+        } catch (error) {
+            return res.status(500).json({ error : error });
+        }
     }
-}
+};
