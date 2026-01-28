ae4d4569d944cac6d643f0ee45268c90f7feb514
suki <88000sofien@gmail.com>
2026-01-28 16:52:26 +0100

    🎨 creation du group de route muscleGroup dans l'index
    (=Integration du Router dans l'index)

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,15 @@
-// @ts-ignore
 import express from 'express';
+import muscleGroupRouter from "./routes/muscleGroupRouter.js";
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json())
 app.get('/', (req: any, res: { json: (arg0: { message: string; }) => void; }) => {
     res.json({ message: "L'API de ton app de sport est prête !" });
 });
 
+app.use("/muscleGroups", muscleGroupRouter)
 app.listen(PORT, () => {
     console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
