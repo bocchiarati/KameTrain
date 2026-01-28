45ad262345318b2cdaddd22379ec54baf45648e6
Ewen <wenwengilbert@gmail.com>
2026-01-29 00:49:51 +0100

    🎨 Exercises Routes : Create GET /exercises/:id

--- api/src/routes/exercisesRouter.ts
+++ api/src/routes/exercisesRouter.ts
@@ -1,7 +1,8 @@
-import { Router } from 'express';
-import { getExercises } from "../controllers/exercisesController.js";
+import {Router} from 'express';
+import {exercisesController} from "../controllers/exercisesController.js";
 
 const router = Router();
-router.get('/', getExercises)
+router.get('/', exercisesController.getExercises)
+router.get('/:id', exercisesController.getExercise)
 
 export default router;
