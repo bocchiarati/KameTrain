7803a92acb8d6061efa1e0ce473462b6fdaa0ec2
bocchiarati <88000sofien@gmail.com>
2026-01-30 21:21:25 +0100

    implement baseRepository

--- api/src/repositories/exercisesRepository.ts
+++ api/src/repositories/exercisesRepository.ts
@@ -1,34 +1,10 @@
-import pool from "../db.js"
-import type {RowDataPacket} from "mysql2";
-import { v4 as uuid } from 'uuid';
+import type {Exercise} from "../models/Exercise.js";
+import {BaseRepository} from "./baseRepository.js";
 
-export const exercisesRepository = {
-    // Récupérer tous les programmes
-    findAll: async (): Promise<RowDataPacket[]> => {
-        const [rows] = await pool.query<RowDataPacket[]>(
-            'SELECT BIN_TO_UUID(id) as id, name, BIN_TO_UUID(muscle_group_id) as muscle_group_id FROM exercise'
-        );
-        return rows;
-    },
-
-    // Récupérer par ID
-    findById: async (id: string): Promise<RowDataPacket | null> => {
-        const [rows]: any = await pool.query(
-            'SELECT BIN_TO_UUID(id) as id, name, BIN_TO_UUID(muscle_group_id) as muscle_group_id FROM exercise WHERE id = UUID_TO_BIN(?)',
-            [id]
-        );
-        return rows[0] || null;
-    },
-
-    create: async (name: string, muscle_group_id: string): Promise<RowDataPacket | null> => {
-        const newId = uuid();
-
-        await pool.query(
-            'INSERT INTO exercise (id, name, muscle_group_id) VALUES (UUID_TO_BIN(?), ?, UUID_TO_BIN(?))',
-            [newId, name, muscle_group_id]
-        );
-
-        // On appelle la méthode du repository pour éviter la duplication
-        return exercisesRepository.findById(newId);
+class ExercisesRepository extends BaseRepository<Exercise> {
+    constructor() {
+        super("exercise"); // Nom de ta table en base de données
     }
-};
+}
+
+export const exercisesRepository = new ExercisesRepository();
--- api/src/repositories/muscleGroupRepository.ts
+++ api/src/repositories/muscleGroupRepository.ts
@@ -1,21 +1,10 @@
-import pool from "../db.js"
-import type {RowDataPacket} from "mysql2";
+import type {Exercise} from "../models/Exercise.js";
+import {BaseRepository} from "./baseRepository.js";
 
-export const muscleGroupRepository = {
-    // Récupérer tous les programmes
-    findAll: async (): Promise<RowDataPacket[]> => {
-        const [rows] = await pool.query<RowDataPacket[]>(
-            'SELECT BIN_TO_UUID(id) as id, libelle FROM muscle_group'
-        );
-        return rows;
-    },
-
-    // Récupérer par ID
-    findById: async (id: string): Promise<RowDataPacket | null> => {
-        const [rows]: any = await pool.query(
-            'SELECT BIN_TO_UUID(id) as id, libelle FROM muscle_group WHERE id = UUID_TO_BIN(?)',
-            [id]
-        );
-        return rows[0] || null;
+class MuscleGroupRepository extends BaseRepository<Exercise> {
+    constructor() {
+        super("muscle_group"); // Nom de ta table en base de données
     }
-};
+}
+
+export const muscleGroupRepository = new MuscleGroupRepository();
