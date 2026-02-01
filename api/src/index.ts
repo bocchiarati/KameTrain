1b3992e4eb768b4f2ce379bf475e8c31711bd1d4
suki <88000sofien@gmail.com>
2026-02-01 18:25:38 +0100

    ✨ Init Users & Programms routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import muscleGroupRouter from "./routes/muscleGroupRouter.js";
-import exercisesRouter from "./routes/exercisesRouter.js";
 import {BaseRouter} from "./routes/baseRouter.js";
 import type {Exercise} from "./models/Exercise.js";
 import type {MuscleGroup} from "./models/MuscleGroup.js";
+import type {Programm} from "./models/Programm.js";
+import type {User} from "./models/User.js";
 
 const app = express();
 const PORT = 3000;
@@ -13,10 +13,10 @@ app.get('/', (req: any, res: { json: (arg0: { message: string; }) => void; }) =>
     res.json({ message: "L'API de ton app de sport est prête !" });
 });
 
-app.use("/muscleGroups", muscleGroupRouter);
-app.use("/exercises", exercisesRouter);
 app.use("/exercises", new BaseRouter<Exercise>("exercise").router);
 app.use("/muscle-groups", new BaseRouter<MuscleGroup>("muscle_group").router);
+app.use("/programms", new BaseRouter<Programm>("programm").router);
+app.use("/users", new BaseRouter<User>("users").router);
 
 app.listen(PORT, () => {
     console.log(`Serveur lancé sur http://localhost:${PORT}`);
---
+++ api/src/models/Programm.ts
@@ -0,0 +1,9 @@
+export interface Programm {
+    id : string,
+    libelle : string,
+    author_id : string,
+    created_at: string,
+    workout_day_number : number,
+    rest_day_number : number,
+    week_number : number
+}
